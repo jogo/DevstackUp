cd703560edb480b8c4a6a1fb860ae34686b079a6
Joe Gordon <jogo@cloudscaling.com>
2013-05-15 15:54:22 -0700

    Checkout grizzly branches

--- load.sh
+++ load.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 sudo apt-get install -qqy git
-git clone https://github.com/openstack-dev/devstack.git;
+git clone -b stable/grizzly https://github.com/openstack-dev/devstack.git ;
 cd devstack;
 sudo cp /vagrant/localrc .
 ./stack.sh
