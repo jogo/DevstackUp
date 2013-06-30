e9268fe29b87e5aaa4d7798f350ffefce6d60b79
Joe Gordon <joe.gordon0@gmail.com>
2013-06-30 04:11:03 -0700

    Improved build-box
    To make 'vagrant up' faster
    
    * Pre-clone git repos
    * Seed pip cache
    * install everything from openstack/requires

--- build-box/build.sh
+++ build-box/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-
+set -e
 vagrant up
+rm package.box
 vagrant package
 vagrant box add devstack package.box -f
 vagrant destroy -f
--- build-box/install-packages.sh
+++ build-box/install-packages.sh
@@ -1,16 +1,27 @@
-#!/bin/bash +x
-
-apt-get install -qqy git
-git clone https://github.com/openstack-dev/devstack.git;
+#!/bin/bash
+set -e
+set -x
+apt-get update
+apt-get install -qqy git vim python-setuptools libpq-dev python-dev libssl-dev libxml2-dev libxslt-dev gir1.2-gnomekeyring-1.0 pkg-config build-essential libnspr4-dev
+easy_install --upgrade pip
+easy_install -U distribute
+git clone --depth=1 https://review.openstack.org/p/openstack-dev/devstack.git
 cd devstack;
-
-TOP_DIR=$(cd $(dirname "$0") && pwd)
-source $TOP_DIR/stackrc
-source $TOP_DIR/functions
-
-install_package $(get_packages $TOP_DIR/files/apts)
-install_package rabbitmq-server
-install_package libvirt-bin
-
+./tools/install_prereqs.sh;
 cd ../
 rm -rf devstack
+
+git clone --depth=1 https://review.openstack.org/p/openstack/requirements.git
+cd  requirements
+mkdir /var/cache/pip
+pip install --download-cache /var/cache/pip -r tools/pip-requires -r tools/test-requires git-review
+cd ../
+rm -rf requirements
+
+
+mkdir /opt/stack
+cd /opt/stack
+for i in nova glance swift cinder horizon keystone python-cinderclient python-glanceclient python-keystoneclient python-novaclient python-quantumclient tempest
+do
+  git clone https://review.openstack.org/p/openstack/$i.git
+done
