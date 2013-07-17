7cdfd9beed7cd40eada8230e6d2b4fc1879de881
Joe Gordon <joe.gordon0@gmail.com>
2013-07-17 00:02:28 -0700

    update for requirements repo

--- build-box/install-packages.sh
+++ build-box/install-packages.sh
@@ -4,7 +4,6 @@ set -x
 apt-get update
 apt-get install -qqy git vim python-setuptools libpq-dev python-dev libssl-dev libxml2-dev libxslt-dev gir1.2-gnomekeyring-1.0 pkg-config build-essential libnspr4-dev
 easy_install --upgrade pip
-easy_install -U distribute
 git clone --depth=1 https://review.openstack.org/p/openstack-dev/devstack.git
 cd devstack;
 ./tools/install_prereqs.sh;
@@ -14,7 +13,7 @@ rm -rf devstack
 git clone --depth=1 https://review.openstack.org/p/openstack/requirements.git
 cd  requirements
 mkdir /var/cache/pip
-pip install --download-cache /var/cache/pip -r tools/pip-requires -r tools/test-requires git-review
+pip install --download-cache /var/cache/pip -r requirements.txt -r test-requirements.txt git-review
 cd ../
 rm -rf requirements
 
